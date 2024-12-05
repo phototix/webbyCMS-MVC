25d747fbcdf40434b7d29d7e014fc4158919bf74
Brandon Chong <phototix@hotmail.com>
2024-12-05 16:49:25 +0800

    Update Autoload.php

--- core/Autoload.php
+++ core/Autoload.php
@@ -4,6 +4,7 @@ class Autoload {
     public static function register() {
         spl_autoload_register(function ($class) {
             $path = __DIR__ . "/../" . str_replace("\\", "/", $class) . ".php";
+            echo $path;
             if (file_exists($path)) {
                 require $path;
             }
