625ed8c71152da88fb6edae13bc727437821f7da
Brandon Chong <phototix@hotmail.com>
2024-12-05 17:38:34 +0800

    Update Autoload.php

--- core/Autoload.php
+++ core/Autoload.php
@@ -3,18 +3,23 @@
 class Autoload {
     public static function register() {
         spl_autoload_register(function ($class) {
-            $path = __DIR__ . "/" . str_replace("\\", "/", $class) . ".php";
-            if (file_exists($path)) {
-                require $path;
+            $paths = [
+                __DIR__ . '/core/',
+                __DIR__ . '/app/Controllers/',
+                __DIR__ . '/app/Models/',
+                __DIR__ . '/app/Views/',
+            ];
+
+            foreach ($paths as $path) {
+                $file = $path . $class . '.php';
+                if (file_exists($file)) {
+                    require_once $file;
+                    return;
+                }
             }
 
-            $classPath = '/app/Controllers/' . $class . '.php';
-            if (file_exists($classPath)) {
-                require_once $classPath;
-            } else {
-                die("Class file for {$class} not found at {$classPath}");
-            }
-
+            // Debugging output for missing classes
+            die("Class file for {$class} not found in registered paths.");
         });
     }
 }
